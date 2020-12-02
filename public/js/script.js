2ac981678a0000abc0b1c2f38cdc89017e077894
takumifukasawa <fukapon912@gmail.com>
2020-12-02 16:17:16 +0900

    register callbacks

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,38 @@
+
 console.log("InteractiveCanvasDebugger");
+
+const canvas = window.interactiveCanvas;
+
+const wrapperElem = document.querySelector(".js-wrapper");
+
+const callbacks = {
+  onUpdate: (data) => {
+    console.log("[onUpdate]", data);
+    try {
+      const dataEntry = data[0];
+      const command = dataEntry.command ? dataEntry.command :
+        dataEntry.google ? dataEntry.google.intent.name : null;
+        const params = dataEntry.displayWord ? dataEntry.displayedWord : null;
+        console.log("[onUpdate: command]", command);
+        console.log("[onUpdate: params]", params);
+    } catch(e) {
+      // catch
+      console.error(e);
+    }
+  },
+  onTtsMark: (markName) => {
+    console.log("[onTtsMark]", markName);
+  },
+};
+
+async function main() {
+  const headerHeightPx = await canvas.getHeaderHeightPx();
+  console.log("[headerHeightPx]", headerHeightPx);
+  console.log("[canvas]", canvas);
+
+  wrapperElem.setAttribute("style", `padding-top: ${headerHeightPx}px;`);
+
+  canvas.ready(callbacks);
+}
+
+main();
